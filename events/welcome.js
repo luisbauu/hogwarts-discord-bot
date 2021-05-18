1c1913aa62027b560b2ed72753bf1a71d274ba93
luisbauu <jose.luis.bautista@obf.ateneo.edu>
2021-05-18 17:22:06 +0800

    bot can now dm new users with welcome letter

---
+++ events/welcome.js
@@ -0,0 +1,18 @@
+module.exports = {
+	// name: 'welcome',
+	// once: false,
+	// execute(client){
+	// 	client.on('guildMemberAdd', member =>{
+	// 		console.log(member);
+
+	// 		const welcomeLetter = new Discord.MessageEmbed()
+	// 		.setColor('#F5D86A')
+	// 		.setTitle('Welcome to Hogwarts')
+	// 		.setDescription(`You are hereby accepted to the Hogwarts School of Witchcraft and Wizardry, ${member}!`)
+	// 		.setImage('https://i.imgur.com/3ZwdT6N.png')
+	// 		.setTimestamp()
+
+	// 		message.channel.send(welcomeLetter);
+	// 	})
+	// }
+};
--- main.js
+++ main.js
@@ -2,40 +2,61 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
 
-
 //// DISCORD PREFIX
 const prefix = '-';
 
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
-
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
+//// COMMAND FILES
 for (const file of commandFiles)
 {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command)
 }
+ 
 
+// EVENT LISTENER
 for (const file of eventFiles)
 {
 	const event = require(`./events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args, client));
-	} else {
+	} 
+	else {
 		client.on(event.name, (...args) => event.execute(...args, client));
 	} 
 }
 
+// const channelId = '843913029556240404';
+// const channel = member.guild.channels.cache.get(channelId);
+
+client.on('guildMemberAdd', member =>{
+	console.log(member);	
+	const user = client.users.cache.get(member);
+	const welcomeLetter = new Discord.MessageEmbed()
+	.setColor('#F5D86A')
+	.setTitle('Welcome to Hogwarts')
+	.setDescription(`You are hereby accepted to the Hogwarts School of Witchcraft and Wizardry, ${member}!`)
+	.setImage('https://i.imgur.com/3ZwdT6N.png')
+	.setTimestamp()
+	
+	// user.send(welcomeLetter);
+	client.users.cache.get(member.id).send(welcomeLetter);
+
+});
+
+
+//// COMMAND HANDLER
 client.on('message', message =>{
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	 
 	if (command === 'bauispogi')
 	{
 		client.commands.get('bauispogi').execute(message,args);
